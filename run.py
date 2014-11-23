b192ad96af0f9a8beaa8dfaefdce0503da73a4ea
Tomasz Warkocki <warkocz@gmail.com>
2014-11-23 01:55:41 +0100

    uncommenting

--- run.py
+++ run.py
@@ -5,11 +5,11 @@ from data_collector import DataCollector
 from data_preprocessor import DataPreprocessor
 from experiment import Experiment
 
-# data_collector = DataCollector('data/tweets', POSITIVE_EMOTICONS + NEGATIVE_EMOTICONS, 30000)
-# data_collector.execute()
+data_collector = DataCollector('data/tweets', POSITIVE_EMOTICONS + NEGATIVE_EMOTICONS, 30000)
+data_collector.execute()
 
-# preprocessor = DataPreprocessor('data/tweets')
-# preprocessor.execute()
+preprocessor = DataPreprocessor('data/tweets')
+preprocessor.execute()
 
 preprocessor = Experiment('data/ready_tweets')
 preprocessor.execute()
